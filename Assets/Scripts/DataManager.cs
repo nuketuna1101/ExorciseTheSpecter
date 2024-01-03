ddba853c06a0d09cc1b71eda8ea297bc550719f8
Jaeyoung <jyko1101@gmail.com>
2024-01-03 22:01:07 +0900

    refactor: graph struct

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,69 +1,97 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
+using static StageChamberSO;
 
 public class DataManager : Singleton<DataManager>
 {
     //
     private string[] fileNames = { "MapChamberInfo" };
 
+
+    [SerializeField]
+    private StageChamberSO _StageChamberSO;
+    private int stageChamberNumber = 13;
+
+    // 인접리스트와 방문노드
+    [SerializeField]
+    public List<int>[] adj;
+    [SerializeField]
+    private bool[] visited;
+
+
+
     private void Awake()
     {
-        TestDataLoad();
-    }
+        ResetChamberInfo();
+        SetChamberInfo(stageChamberNumber);
 
-    private void TestDataLoad()
-    {
-        var dataList = new List<Dictionary<string, object>>();
-        string file = "MapChamberInfo";
-        dataList = CSVReader.Read(file);
-
-        DebugOpt.Log("test code ::" + int.Parse(dataList[0]["TempCol"].ToString()));
-        DebugOpt.Log("test code ::" + int.Parse(dataList[1]["TempCol"].ToString()));
-        DebugOpt.Log("test code ::" + int.Parse(dataList[2]["TempCol"].ToString()));
-        DebugOpt.Log("test code ::" + int.Parse(dataList[3]["TempCol"].ToString()));
-
-        /*
-        for (int index = 0; index < _AreaFixedDatas.Length; index++)
-        {
-
-            AreaFixedData curArea = _AreaFixedDatas[index];
-            curArea.areaID = int.Parse(dataList[index]["areaType"].ToString());
-            curArea.prevAreaID = int.Parse(dataList[index]["openCondition"].ToString());
-        }
-        */
-    }
-
-
-    private void ManageMapChamberInfo()
-    {
-        //
-        var dataList = new List<Dictionary<string, object>>();
-        string file = "MapChamberInfo";
-        dataList = CSVReader.Read(file);
+        InitEdge();
 
     }
 
-    private void SetByStageNumber(int _StageNumber)
+    // StageChamberSO 초기화
+    private void ResetChamberInfo()
     {
-        //
-        var dataList = new List<Dictionary<string, object>>();
-        string file = "MapChamberInfo";
-        dataList = CSVReader.Read(file);
-
-        //
-        // 현재 so에 대해
         
+        _StageChamberSO.StageChamberArray.Clear();
+    }
+    // CSV로부터 StageChamberSO 저장
+    private void SetChamberInfo(int _StageChamberNumber)
+    {
+        //
+        var dataList = new List<Dictionary<string, object>>();
+        string file = "MapChamberInfo";
+        dataList = CSVReader.Read(file);
 
-        /*
-        for (int index = 0; index < _AreaFixedDatas.Length; index++)
+        _StageChamberSO.StageChamberArray.Add( new ChamberArray
         {
-
-            AreaFixedData curArea = _AreaFixedDatas[index];
-            curArea.areaID = int.Parse(dataList[index]["areaType"].ToString());
-            curArea.prevAreaID = int.Parse(dataList[index]["openCondition"].ToString());
+            StageNumber = 0,
+            ChamberNumber = 0,
+            ChamberType = 0,
+            NextChamber1 = 0,
+            NextChamber2 = 0,
         }
-        */
+        );
+
+
+        for (int i = 0; i < _StageChamberNumber; i++)
+        {
+            _StageChamberSO.StageChamberArray.Add( new ChamberArray
+            {
+                StageNumber = CSVReader.GetIntValue(dataList, i, "StageNumber"),
+                ChamberNumber = CSVReader.GetIntValue(dataList, i, "ChamberNumber"),
+                ChamberType = CSVReader.GetIntValue(dataList, i, "ChamberType"),
+                NextChamber1 = CSVReader.GetIntValue(dataList, i, "NextChamber1"),
+                NextChamber2 = CSVReader.GetIntValue(dataList, i, "NextChamber2"),
+            }
+            );
+        }
+
+    }
+
+
+
+    private void InitEdge()
+    {
+        // 스테이지 넘버에 해당하는 데이터에 따라 맵 세팅
+
+        // 데이터 테이블로부터 사전 세팅
+        // 인접 엣지 리스트 adj 세팅
+        adj = new List<int>[stageChamberNumber + 1];
+        for (int i = 1; i <= stageChamberNumber; i++)
+        {
+            adj[i] = new List<int>();
+            if (_StageChamberSO.StageChamberArray[i].NextChamber1 != -1)
+                adj[i].Add(_StageChamberSO.StageChamberArray[i].NextChamber1);
+            if (_StageChamberSO.StageChamberArray[i].NextChamber2 != -1)
+                adj[i].Add(_StageChamberSO.StageChamberArray[i].NextChamber2);
+        }
+        // 방문 배열 visited 세팅
+        visited = new bool[stageChamberNumber + 1];
+        Array.Fill(visited, false);
     }
 }
--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -19,22 +19,5 @@ public class MapManager : MonoBehaviour
     private bool[] visited;
 
 
-    private void InitEdge()
-    {
-        // 현재 스테이지 넘버 가져오기.
-        StageNumber = GameManager.Instance.StageNumber;
-        int numberofstageChambers= 25;
-        // 스테이지 넘버에 해당하는 데이터에 따라 맵 세팅
-
-        // 데이터 테이블로부터 사전 세팅
-        adj = new List<int>[numberofstageChambers + 1];
-        visited = new bool[numberofstageChambers + 1];
-        Array.Fill(visited, false);
-
-        //
-
-
-    }
-
 
 }
